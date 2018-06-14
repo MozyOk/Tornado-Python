c796ce9cd9fc639213ca982f3dfa9d2eac4eff11
MozyOk <mozy.okubo@gmail.com>
2018-06-14 23:43:56 +0900

    add: database

--- todo/__init__.py
+++ todo/__init__.py
@@ -5,14 +5,21 @@ from tornado.options import define, options
 from tornado.web import Application
 from todo.views import HelloWorld
 
+# add these
+import os
+from tornado_sqlalchemy import make_session_factory
+
 define('port', default=8888, help='port to listen on')
+factory = make_session_factory(os.environ.get('DATABASE_URL', ''))
 
 
 def main():
     """Construct and serve the tornado application."""
     app = Application([
         ('/', HelloWorld)
-    ])
+    ],
+        session_factory=factory
+    )
     http_server = HTTPServer(app)
     http_server.listen(options.port)
     print('Listening on http://localhost:%i' % options.port)
---
+++ todo/models.py
@@ -0,0 +1,8 @@
+# this is not the complete models.py, but enough to see the differences
+from tornado_sqlalchemy import declarative_base
+
+Base = declarative_base
+
+
+class Task(Base):
+    # and so on, because literally everything's the same...
