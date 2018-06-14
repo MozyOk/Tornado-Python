b0f29c13283719e3d79ab865d2e8777fa577f21d
MozyOk <mozy.okubo@gmail.com>
2018-06-14 23:46:20 +0900

    add: prepare

--- todo/views.py
+++ todo/views.py
@@ -7,6 +7,12 @@ from tornado.web import RequestHandler
 class BaseView(RequestHandler, SessionMixin):
     """Base view for this application."""
 
+    def prepare(self):
+        self.form_data = {
+            key: [val.decode('utf8') for val in val_list]
+            for key, val_list in self.request.arguments.items()
+        }
+
     def set_default_headers(self):
         """Set the default response header to be JSON."""
         self.set_header("Content-Type", 'application/json; charset="utf-8"')
